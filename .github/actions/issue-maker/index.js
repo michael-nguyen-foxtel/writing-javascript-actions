55fbe22521c77ec1b89fbfb51bbe5c1a24eefd30
nguyenm <michael.nguyen@foxtel.com.au>
2022-01-24 18:10:41 +1100

    Changed according to updated package usage

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -3,18 +3,18 @@ const github = require("@actions/github");
 
 async function run() {
     try {
-      const issueTitle = core.getInput('issue-title');
-      const jokeBody = core.getInput("joke");  
-      const token = core.getInput("repo-token");
+        const issueTitle = core.getInput("issue-title");
+        const jokeBody = core.getInput("joke");  
+        const token = core.getInput("repo-token");
 
-      const octokit = new github.getOctokit(token);
+        const octokit = new github.getOctokit(token);
 
-      const newIssue = await octokit.issues.create({
-          repo: github.context.repo.repo,
-          owner: github.context.repo.owner,
-          title: issueTitle,
-          body: jokeBody
-      });
+        const newIssue = await octokit.rest.issues.create({
+            repo: github.context.repo.repo,
+            owner: github.context.repo.owner,
+            title: issueTitle,
+            body: jokeBody
+        });
     } catch (err) {
         core.setFailed(err.message);
     }
